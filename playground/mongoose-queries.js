d132a463fae5af0d05f76edae8b11a4bffb5d441
Shane <sdarcy@redventures.com>
2017-09-27 00:13:34 -0400

    Add queries

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,16 +1,39 @@
+const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
 
-var id = "59cac0bfb2d4b82f9c80f6e5";
+// var id = "59cac0bfb2d4b82f9c80f6e5";
 
-Todo.find({
-    _id: id
-}).then((todos) => {
-    console.log('Todos', todos);
-});
+// if (!ObjectId.isValid(id)) {
+//     console.log('ID is not valid');
+// }
 
-Todo.findOne({
-    _id: id
-}).then((todo) => {
-    console.log('Todo', todo);
-});
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+//The code below indicated that the query works, but there is no user
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+var id = '59cb23019a4401546f072929';
+
+User.findById(id).then((user) => {
+    if (!user) {
+        return console.log('Could not find that user');
+    } 
+    console.log('User by Id', user);
+}).catch((e) => console.log(e));
